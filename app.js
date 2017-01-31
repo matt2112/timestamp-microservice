092bcfb0ab6e29f8b998d13982427e2a9b461ca7
Matt Lewis <matthew.lewis@pagesuite.com>
2017-01-31 21:06:59 +0000

    configure for heroku deployment

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 const express = require('express');
 const app = express();
 
-const port = process.argv[2] || 3000;
+const port = process.env.PORT || 3000;
 
 // If no date parameter passed to API, then just display the static index.html.
 app.use(express.static(__dirname + '/public'));
